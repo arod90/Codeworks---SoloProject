c52d71b571448a3668c741b5ce6b5ad71c0d6ee9
Andres Rodriguez <web.arod@gmail.com>
2022-07-12 12:41:06 +0200

    added google logo and cleaned up assest folder

--- src/components/LoginReg.jsx
+++ src/components/LoginReg.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
 import { UserAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
-import glogo from '../assets/glogo.png';
 import '../styles/LoginReg.css';
+import { FcGoogle } from 'react-icons/fc';
 
 const LoginReg = () => {
   const { googleSignIn, user } = UserAuth();
@@ -30,8 +30,7 @@ const LoginReg = () => {
         <h1 className="h1-back">TRVL</h1>
         <div className="form-cont">
           <button className="g-singin" onClick={handleGoogleSignIn}>
-            <p>Sign-in with Google</p>{' '}
-            <img className="glogo" src={glogo} alt="" />
+            <p>Sign-in with Google</p> <FcGoogle size={35} />
           </button>
           <h2 onClick={guestView}>Or continue as a Guest</h2>
         </div>
