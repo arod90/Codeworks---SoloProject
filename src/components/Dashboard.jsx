f9a8a0f1248c10b80506762a1a88adca26466091
Andres Rodriguez <web.arod@gmail.com>
2022-07-12 12:37:10 +0200

    added loading screen

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,45 +1,64 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
 import '../styles/Dashboard.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import load from '../assets/load.svg';
 
 const Dashboard = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 4000);
+  }, []);
+
   return (
     <>
-      <section className="main-section">
-        <div className="title-cont">
-          <div className="text-cont">
-            <div className="h1-cont">
-              <div className="title-cont1">
-                <div className="t-cont">
-                  <h1 className="letter">T</h1>
+      {!isLoading ? (
+        <section className="main-section">
+          <div className="title-cont">
+            <div className="text-cont">
+              <div className="h1-cont">
+                <div className="title-cont1">
+                  <div className="t-cont">
+                    <h1 className="letter">T</h1>
+                  </div>
+                  <div className="r-cont">
+                    <h1 className="letter">R</h1>
+                  </div>
                 </div>
-                <div className="r-cont">
-                  <h1 className="letter">R</h1>
+                <div className="title-cont2">
+                  <div className="v-cont">
+                    <h1 className="letter">V</h1>
+                  </div>
+                  <div className="l-cont">
+                    <h1 className="letter">L</h1>
+                  </div>
                 </div>
               </div>
-              <div className="title-cont2">
-                <div className="v-cont">
-                  <h1 className="letter">V</h1>
-                </div>
-                <div className="l-cont">
-                  <h1 className="letter">L</h1>
-                </div>
+              <div className="subtitle-cont">
+                <h4>ADVISOR</h4>
               </div>
-            </div>
-            <div className="subtitle-cont">
-              <h4>ADVISOR</h4>
             </div>
           </div>
-        </div>
-        <div className="btn-cont">
-          <Link className="link" to="/loginreg">
-            <button className="start">Get Started</button>
-          </Link>
-          {/* <Link to="/stack">
+          <div className="btn-cont">
+            <Link className="link" to="/loginreg">
+              <button className="start">Get Started</button>
+            </Link>
+            {/* <Link to="/stack">
             <span className="hidden-dev">👨🏽‍💻</span>
           </Link> */}
+          </div>
+        </section>
+      ) : (
+        <div className="loading-page">
+          <h1>Loading TRVL Advisor</h1>
+          <img src={load} alt="" />
         </div>
-      </section>
+      )}
     </>
   );
 };
